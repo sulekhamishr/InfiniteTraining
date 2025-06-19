07f5a48dbd179c408bf5d3547c0b0ea95d6ba1cd
sulekhamishr <msulekha277@gmail.com>
2025-06-19 15:15:44 +0530

    Add files via upload

---
+++ Demo/src/com/java/demo/A.java
@@ -0,0 +1,15 @@
+package com.java.demo;
+
+public class A {
+
+	
+	public static void main(String[] args) {
+	  int a= 10;
+	  int b=20;
+	try {
+		throw new MyException("Error get")
+	}catch(MyException e) {
+		
+	}
+}
+}
